32f198fec561ffeb5ac90822e22ef8025158f2e0
양우석 <woosuk288@naver.com>
2022-06-11 11:36:29 +0900

    CLN: console.log

--- src/landing/KakaoShareButton.tsx
+++ src/landing/KakaoShareButton.tsx
@@ -18,7 +18,7 @@ export const KakaoShareButton = ({ url }: KakaoShareButtonProps) => {
       // 중복 initialization 방지
       if (!kakao.isInitialized()) {
         // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-        kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
+        kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY);
       }
       kakao.Link.createDefaultButton({
         // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
