7463be4a418bb26b779cfc9e86a635cbe95757bf
matrix <woosuk288@gmail.com>
2022-09-21 15:31:57 +0900

    TST: firestore emulator 설정

--- src/utils/firebase/firebaseInit.ts
+++ src/utils/firebase/firebaseInit.ts
@@ -3,7 +3,7 @@ import { ConfirmationResult, getAuth, RecaptchaVerifier } from 'firebase/auth';
 import { getAnalytics, isSupported } from 'firebase/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { getFirestore } from 'firebase/firestore';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -25,6 +25,20 @@ auth.useDeviceLanguage();
 
 const db = getFirestore(app);
 const storage = getStorage(app);
+
+const EMULATORS_STARTED = 'EMULATORS_STARTED';
+function startEmulators() {
+  // @ts-ignore
+  if (!global[EMULATORS_STARTED]) {
+    // @ts-ignore
+    global[EMULATORS_STARTED] = true;
+    connectFirestoreEmulator(db, 'localhost', 8080);
+  }
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  startEmulators();
+}
 
 export { app, auth, analytics, db, storage };
 export const useAuthFb = () => useAuthState(auth);
