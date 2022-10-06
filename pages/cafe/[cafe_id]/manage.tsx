390e86d57021455e4b58f14e989207d3350dd355
matrix <woosuk288@gmail.com>
2022-10-06 21:41:34 +0900

    TST: 바바카멜 2차 이미지 데이터 추가

--- pages/cafe/[cafe_id]/manage.tsx
+++ pages/cafe/[cafe_id]/manage.tsx
@@ -64,51 +64,58 @@ function Manage() {
     if (token?.claims.admin !== true) return;
 
     const images = await getImageURLs(prefix, filename, suffix);
+
     const result = await createMenu(data, images);
     console.log('data : ', { ...data, images });
     alert(result);
   };
 
   const handleUdpateMenuImages = () => {
-    const path = `cafes/${data.cafeId}/menus/${data.id}`;
-    const prefix = 'images/menus/babacarmel/';
-    const filename = '';
-    const suffix = '.jpg';
+    // const path = `cafes/${data.cafeId}/menus/${data.id}`;
+    // const prefix = 'images/menus/babacarmel/';
+    // const filename = '';
+    // const suffix = '.jpg';
     updateImages(path, prefix, filename, suffix);
   };
 
   return (
     <>
       <div style={{ whiteSpace: 'pre' }}>{JSON.stringify(data, null, 4)}</div>
-      <div>
+      <Button variant="contained" onClick={handleMenuCreate}>
+        메뉴 추가
+      </Button>
+
+      {/* <div>
+        docPath: {path} <br />
         {prefix}
         {filename}
         {suffix}
-      </div>
-
-      <Button onClick={handleMenuCreate}>메뉴 추가</Button>
-
-      <Button onClick={handleUdpateMenuImages}>사진 업데이트</Button>
+      </div> */}
+      {/* <Button variant="contained" onClick={handleUdpateMenuImages}>사진 업데이트</Button> */}
     </>
   );
 }
 
+const cafeId = 'babacarmel';
+const menuId = 'babacarmel-416';
+
 const data = {
-  id: 'babacarmel-118',
-  cafeId: 'babacarmel',
-  name: '흑임자 카페라떼',
+  id: menuId,
+  cafeId,
+  name: '악마의 로투스 르뱅 쿠키',
   description: '',
   labels: [],
   imageURL: '/images/logo_icon.png',
   images: {} as ImagesType,
-  price: 5500,
-  category: '에스프레소',
+  price: 3800,
+  category: '디저트',
   reviewCount: 0,
   createdAt: new Date(),
 };
 
+const path = `cafes/${cafeId}/menus/${menuId}`;
 const prefix = `images/menus/${data.cafeId}/`;
-const filename = '에스프레소 - 흑임자카페라떼';
+const filename = '디저트 - 악마의 로투스 르뱅쿠키';
 const suffix = '.jpg';
 
 // const data = {
